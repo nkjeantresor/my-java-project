ba259ed32342a0e3f0f2fa72be7b07b71574d3e9
nkjeantresor <149670102+nkjeantresor@users.noreply.github.com>
2023-11-10 11:56:58 +0200

    Add files via upload

---
+++ tesoro.java
@@ -0,0 +1,11 @@
+// this program display the addition of two numbers
+public class tesoro {
+public static void main(String[] args) {
+    
+    int a=2;//  declaring and initialising a to 2 and b to 5
+    int b=5;
+    int d=a+b;
+    System.out.println("the sum is" );
+    System.out.print(d);
+}
+}
