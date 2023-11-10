f74c15443cd01854c51345d53165c1c13c8ad7ce
nkjeantresor <149670102+nkjeantresor@users.noreply.github.com>
2023-11-10 12:01:54 +0200

    Add files via upload

---
+++ function.java
@@ -0,0 +1,17 @@
+// this program uses method in addition of two numbers
+public class function {
+     public static int addNumbers(int a, int b) {
+        return a + b;// this function that is made is going to return the addition of two numbers
+    }
+    public static void main(String[] args) {
+        int num1 = 5;//declaring  and initializing the new numbers to be used as num1 and num2
+        int num2 = 7;
+
+        int sum = addNumbers(num1, num2);
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+
+    // Function to add two numbers
+   
+}
